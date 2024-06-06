98e58899881f3a6a3fc6df3df8b150c85a9e729d
Josiel <josiel.borges@sc.senai.br>
2024-06-06 15:28:38 -0300

    Improved code

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect
 
 
 class Game():
@@ -10,13 +8,16 @@ class Game():
         self.console = console
 
 
-@app.route('/')
-def main():
-    game1 = Game(name='Resident Evil', category='Action', console='PS4')
-    game2 = Game(name='Metroid', category='RPG', console='Switch')
+game1 = Game(name='Resident Evil', category='Action', console='PS4')
+game2 = Game(name='Metroid', category='RPG', console='Switch')
+games_list = [game1, game2]
 
-    games = [game1, game2]
-    return render_template('gamelist.html', title='Teste', games=games)
+app = Flask(__name__)
+
+
+@app.route('/')
+def games():
+    return render_template('gamelist.html', title='Teste', games=games_list)
 
 
 @app.route('/new')
@@ -24,4 +25,16 @@ def new_game():
     return render_template('new_game.html', title='New Game')
 
 
-app.run()
+@app.route('/create', methods=['POST'])
+def create_game():
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+
+    game = Game(name, category, console)
+    games_list.append(game)
+
+    return redirect('/')
+
+
+app.run(debug=True)
