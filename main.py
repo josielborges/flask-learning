22124ecd978c6370069ffa75784e262c8cc79b9d
Josiel <josiel.borges@sc.senai.br>
2024-06-06 14:15:31 -0300

    Created 2 simples pages to test the use of python on html

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+class Game():
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
+
 @app.route('/')
-def hello():
-    return '<h1>Hello World!</h1>'
+def main():
+    game1 = Game(name='Resident Evil', category='Action', console='PS4')
+    game2 = Game(name='Metroid', category='RPG', console='Switch')
+
+    games = [game1, game2]
+    return render_template('gamelist.html', title='Teste', games=games)
+
+
+@app.route('/new')
+def new_game():
+    return render_template('new_game.html', title='New Game')
+
 
 app.run()
